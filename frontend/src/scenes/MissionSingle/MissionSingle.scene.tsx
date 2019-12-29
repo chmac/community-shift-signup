1a88931bd8a452b351deb0c267e51bcf770e9841
Callum Macdonald <git@callum-macdonald.com>
2019-12-29 15:30:09 +0100

    Skeleton mission single.

--- frontend/src/scenes/MissionSingle/MissionSingle.scene.tsx
+++ frontend/src/scenes/MissionSingle/MissionSingle.scene.tsx
@@ -1,8 +1,54 @@
 import React from "react";
+import gql from "graphql-tag";
 import { RouteComponentProps } from "react-router-dom";
+import { useQuery } from "react-apollo";
+
+const MissionSingleQuery = gql`
+  query MissionSingleQuery($id: uuid!) {
+    missions_by_pk(id: $id) {
+      id
+      user_id
+      name
+      slug
+      description_md
+    }
+  }
+`;
 
 const MissionSingle = (props: Props) => {
-  return <div>MissionSingle {props.match.params.id}</div>;
+  const { data, error, loading } = useQuery<{
+    missions_by_pk?: {
+      id: string;
+      user_id: string;
+      name: string;
+      slug: string;
+      description_md: string;
+    };
+  }>(MissionSingleQuery, {
+    variables: { id: props.match.params.id }
+  });
+
+  if (error) {
+    return <div>Error #dYQVlJ: ${error.message}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
+  if (!data?.missions_by_pk) {
+    return <div>Not found #q8qJBh</div>;
+  }
+
+  const mission = data.missions_by_pk;
+
+  return (
+    <div>
+      <h1>{mission.name}</h1>
+      <p>ID: {mission.id}</p>
+      <p>Description: {mission.description_md}</p>
+    </div>
+  );
 };
 
 interface RouteParams {
